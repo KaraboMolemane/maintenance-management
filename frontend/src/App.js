569f09e59e9b9e924288cb8ab201a2329012ac6e
KaraboMo <karabo.molemane@adaptit.com>
2023-09-09 13:02:55 +0200

    bulk edit modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import DataGrid, {
   Form,
   HeaderFilter,
   Search,
+  CheckBox,
+  SelectBox,
 } from "devextreme-react/data-grid";
 import "devextreme-react/text-area";
 import { Item } from "devextreme-react/form";
@@ -23,6 +25,13 @@ function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [allJobs, setAllJobs] = useState([]);
+
+  const [state, setState] = useState({
+    selectTextOnEditStart: true,
+    startEditAction: "click",
+  });
+
   const editingModeID = useRef(0);
 
   const status = [
@@ -55,6 +64,24 @@ function App() {
     },
   ];
 
+  const archived = [
+    {
+      ID: "true",
+      Name: "true",
+    },
+    {
+      ID: "false",
+      Name: "false",
+    },
+  ];
+
+  const startEditActions = ["click", "dblClick"];
+  const actionLabel = { "aria-label": "Action" };
+
+  const onSelectTextOnEditStartChanged = function () {
+    // TO DO
+  };
+
   useEffect(() => {
     //Do the API call
     fetch("/get-open-jobs")
@@ -77,10 +104,11 @@ function App() {
   const onSaving = useCallback((e) => {
     console.log("On saving", e);
 
-    if(e.changes[0].type === "remove"){
-      // Archive job 
-      // DO NOT delete 
-      const job = {id: e.changes[0].key }
+
+    if (e.changes[0].type === "remove") {
+      // Archive job
+      // DO NOT delete
+      const job = { id: e.changes[0].key };
       fetch("/archive-job", {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
@@ -90,21 +118,19 @@ function App() {
         editingModeID.current = 0; //Reset editingMode,
         window.location.href = "/";
       });
-
-    }
-    else{
-      // Edit or add new job 
+    } else {
+      // Edit or add new job
       const job = {
         description: e.changes[0].data.description,
         location: e.changes[0].data.location,
         priority: e.changes[0].data.priority,
-        status: e.changes[0].data.status
+        status: e.changes[0].data.status,
       };
-  
+
       if (e.changes[0].type === "update") {
         // EDIT existing job
         job.id = editingModeID.current;
-  
+
         fetch("/edit-job", {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
@@ -125,7 +151,6 @@ function App() {
           window.location.href = "/";
         });
       }
-
     }
   }, []);
 
@@ -151,6 +176,23 @@ function App() {
     //e.promise = saveChange(dispatch, e.changes[0]);
   }, []);
 
+  function loadModalData() {
+    fetch("/get-all-jobs")
+      //.then((res) => console.log('res:',res))
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          console.log("All Jobs:", result);
+          setAllJobs(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          console.log("error:", error);
+          setError(error);
+        }
+      );
+  }
 
   return (
     <>
@@ -198,6 +240,95 @@ function App() {
           <Column dataField="createdAt" dataType="date" />
           <Column dataField="updatedAt" dataType="date" />
         </DataGrid>
+      </div>
+      {/* modal for buld edit*/}
+      <div className="d-inline-flex gap-2 mb-5 mt-5">
+        <button
+          type="button"
+          className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModalFullscreen"
+          onClick={() => loadModalData()}
+        >
+          Bulk edit
+        </button>
+      </div>
+      <div
+        className="modal fade"
+        id="exampleModalFullscreen"
+        tabIndex="-1"
+        aria-labelledby="exampleModalFullscreenLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-fullscreen">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-4" id="exampleModalFullscreenLabel">
+                Bulk edit
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div id="data-grid-demo">
+                <DataGrid
+                  dataSource={allJobs}
+                  keyExpr="_id"
+                  showBorders={true}
+                  onSaving={onSaving}
+                  onSaved={afterSaving}
+                  onInitNewRow={addNewJob}
+                  onEditingStart={editJob}
+                >
+                  <Paging enabled={false} />
+                  <Editing
+                    mode="batch"
+                    allowUpdating={true}
+                    // selectTextOnEditStart={state.selectTextOnEditStart}
+                    // startEditAction={state.startEditAction}
+                  />
+                  <Column dataField="_id" caption="Id" width={70} />
+                  <Column dataField="description" width={170} />
+                  <Column dataField="location" />
+                  <Column dataField="archived" caption="Archived">
+                    <Lookup
+                      dataSource={archived}
+                      displayExpr="Name"
+                      valueExpr="ID"
+                    />
+                  </Column>
+                  <Column dataField="status" caption="Status">
+                    <Lookup
+                      dataSource={status}
+                      displayExpr="Name"
+                      valueExpr="ID"
+                    />
+                  </Column>
+                  <Column dataField="priority" caption="Priority">
+                    <Lookup
+                      dataSource={priority}
+                      displayExpr="Name"
+                      valueExpr="ID"
+                    />
+                  </Column>
+                </DataGrid>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
